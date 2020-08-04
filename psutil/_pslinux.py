0ce8bcb7bd70ea62e9b22589c69ad922cbb7379d
Ross1503 <46737305+Ross1503@users.noreply.github.com>
2020-08-04 18:05:54 +0530

    Update _pslinux.py

--- psutil/_pslinux.py
+++ psutil/_pslinux.py
@@ -493,8 +493,8 @@ def virtual_memory():
 
 
 def swap_memory():
-    globle total
-    globle free
+    global total
+    global free
     """Return swap memory metrics."""
     #mems = {}
     with open_binary('%s/meminfo' % get_procfs_path()) as f:
